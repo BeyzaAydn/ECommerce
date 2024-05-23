b06cda563fad2d337070e0ae1523d20b94dc2843
BeyzaAydn <av.aysebeyzakucuk@gmail.com>
2024-05-23 17:31:13 +0300

    Add project files.

---
+++ ECommerce.Model/Base/BaseEntity.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Model.Enums;
+
+namespace ECommerce.Model.Base
+{
+    public class BaseEntity
+    {
+
+        public BaseEntity() 
+        {
+
+        
+        
+        }
+        public int Id { get; set; }
+
+        public Guid MasterId { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        public string CreatedComputerName { get; set; }
+
+        public string CreatedIpAddress { get; set; }
+
+        public DateTime UpdatedTime { get; set; }
+
+        public string UpdatedComputerName { get; set; }
+        public string UpdatedIpAddress { get; set; }
+
+        public DataStatus DataStatus { get; set; }
+
+        public bool IsActive { get; set; }
+
+
+
+
+    }
+}
